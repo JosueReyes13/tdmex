3ebc455ddd42f140dc263623a5490d08f0c59e96
JosueReyes13 <petbook51@hotmail.com>
2026-01-09 17:45:47 -0600

    googleads

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,27 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    // Agregamos suppressHydrationWarning para ignorar cambios de extensiones
+    <html lang="es" suppressHydrationWarning>
       <head>
         <title>TDMEX</title>
         <link rel="icon" type="image/png" href="/fotos/TDMEX_ICON.png" />
+        
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P4BBV5PD');`,
+          }}
+        />
       </head>
       <body
         style={{
@@ -18,13 +32,17 @@ export default function RootLayout({ children }) {
           minHeight: "100vh",
         }}
       >
-        {/* Navbar se mostrará en todas las páginas */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P4BBV5PD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Navbar />
-
-        {/* Contenido específico de cada page */}
         <main style={{ flex: 1 }}>{children}</main>
-
-        {/* Footer al final */}
         <Footer />
       </body>
     </html>
